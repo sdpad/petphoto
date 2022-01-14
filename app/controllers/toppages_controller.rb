5a80fd53c3697944884f11f090a32389ffefbcd6
sd.ka <sdpad75@gmail.com>
2022-01-14 11:30:38 +0000

    modified toppage

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,7 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @posts = current_user.feed_posts.order(id: :desc).page(params[:page]).per(9)
+    end
   end
 end
